7a9c482cb0cc0615ce958f1cce090dd9f0fb9547
avillao <villaoaron@gmail.com>
2021-08-12 15:18:22 -0500

    Se agregaron los componentes: plan y comparePlan

--- Proyecto/src/app/app.module.ts
+++ Proyecto/src/app/app.module.ts
@@ -6,13 +6,17 @@ import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
 import { LoginComponent } from './login/login.component';
 import { NavComponent } from './componentes/pagina_comercial/nav/nav.component';
+import { PlanComponent } from './componentes/pagina_comercial/plan/plan.component';
+import { ComparePlanComponent } from './componentes/pagina_comercial/compare-plan/compare-plan.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HomeComponent,
     LoginComponent,
-    NavComponent
+    NavComponent,
+    PlanComponent,
+    ComparePlanComponent
   ],
   imports: [
     BrowserModule,
