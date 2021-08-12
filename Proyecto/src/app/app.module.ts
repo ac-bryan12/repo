237f190ffbf0659d164ab62d26ba2320de88424f
Bryan Alava <bpalava@espol.edu.ec>
2021-08-12 14:31:57 -0500

    Manejo de rutas y creacion de componente nav_bar para la navegacion xd

--- Proyecto/src/app/app.module.ts
+++ Proyecto/src/app/app.module.ts
@@ -5,12 +5,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
 import { LoginComponent } from './login/login.component';
+import { NavComponent } from './componentes/pagina_comercial/nav/nav.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HomeComponent,
-    LoginComponent
+    LoginComponent,
+    NavComponent
   ],
   imports: [
     BrowserModule,
