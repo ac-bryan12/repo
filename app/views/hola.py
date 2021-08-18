b1d9af10508c79eb26ff833031aa9d237612a5f4
avillao <villaoaron@gmail.com>
2021-08-18 13:14:23 -0500

    Creación y configuración del proyecto.

---
+++ app/views/hola.py
@@ -0,0 +1,12 @@
+from django.conf import urls
+from django.shortcuts import render
+from django.views.decorators.csrf import csrf_exempt
+from django.urls import path
+
+@csrf_exempt
+def saludo(request):
+    return render(request,'hola.html')
+
+urlpatterns = [
+    path('',saludo)
+]
