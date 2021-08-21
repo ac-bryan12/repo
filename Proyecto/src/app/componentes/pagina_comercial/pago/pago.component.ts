ad7505c62cf830532a57a1248e11b4ef7ef19904
raochoa019 <raochoa@espol.edu.ec>
2021-08-20 23:51:06 -0500

    Cambios pago, confirmacion, registro

--- Proyecto/src/app/componentes/pagina_comercial/pago/pago.component.ts
+++ Proyecto/src/app/componentes/pagina_comercial/pago/pago.component.ts
@@ -6,6 +6,38 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./pago.component.css']
 })
 export class PagoComponent implements OnInit {
+  
+  mostrarFormasPago() {
+    let pagos = document.getElementById('botones-tarjetas') as HTMLElement;
+    pagos.setAttribute("style","display: none");
+    let cont = document.getElementById('metodos-pago') as HTMLElement;
+    cont.setAttribute("style","display: inline");
+  }
+
+  volverFormaPago(){
+    let cont = document.getElementById('metodos-pago') as HTMLElement;
+    cont.setAttribute("style","display: inline");
+    let cancel = document.getElementById('pay-creditcard') as HTMLElement;
+    cancel.setAttribute("style","display: none");
+  }
+
+  mostrarPagoTarjeta() {
+    let seccion = document.getElementById('metodos-pago') as HTMLElement;
+    seccion.setAttribute("style","display: none");
+    let cont = document.getElementById('pay-creditcard') as HTMLElement;
+    cont.setAttribute("style","display: inline");
+  }
+
+  mostrarPagoTransf(){
+    let sec = document.getElementById('metodos-pago') as HTMLElement;
+    sec.setAttribute("style","display: none");
+    let cont = document.getElementById('pay-tranfer') as HTMLElement;
+    cont.setAttribute("style","display: inline");
+    
+
+  }
+
+  
 
   constructor() { }
 
