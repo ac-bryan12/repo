ab07e76b403dbf1f59ab68550a1f83d5911ee315
avillao <villaoaron@gmail.com>
2021-08-23 17:48:50 -0500

    Mejorando el envio del formulario de creación de cuenta de empresa

--- Proyecto/src/app/componentes/pagina_comercial/create-cuenta/create-cuenta.component.ts
+++ Proyecto/src/app/componentes/pagina_comercial/create-cuenta/create-cuenta.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { LoginService } from 'src/app/services/login.service';
-import { FormBuilder, FormGroup, Validators} from '@angular/forms';
+import { FormBuilder, FormGroup, Validators,FormControlName,FormArrayName} from '@angular/forms';
 
 
 @Component({
@@ -12,30 +12,40 @@ import { FormBuilder, FormGroup, Validators} from '@angular/forms';
 export class CreateCuentaComponent implements OnInit {
   public createAccount: FormGroup;
   
-  objeto = {
-    Organizacion: { ruc: '', correoorg: '', razon: '', telefono: '', direccion: '' },
-    Usuario: { nombre: '', apellido: '', cargo: '', contrasena: '', correouser: '', valcontrasena: '' }
-  }
   constructor(
     private envio: LoginService,
     private fb: FormBuilder
   ) {
     this.createAccount = this.fb.group({
-      ruc: this.fb.control('', [Validators.required,Validators.pattern('^[0-9]+$'),Validators.maxLength(13),Validators.minLength(13)]),
-      mail: this.fb.control('', [Validators.required,Validators.pattern('^[a-zA-Z0-9_.]+@[a-zA-Z].+[a-zA-Z0-9]$'),Validators.minLength(7)]),
-      razonsocial: this.fb.control('', [Validators.required,Validators.pattern('^[a-zA-Z_ ]+$'),Validators.minLength(5), Validators.maxLength(20)]),
-      telefono: this.fb.control('', [Validators.required,Validators.pattern('^[+_0-9]+$'),Validators.minLength(13),Validators.maxLength(20)]),
-      direccion: this.fb.control('', [Validators.required,Validators.pattern('^[a-zA-Z0-9._ ]+$')]),
+      empresa: this.fb.group({
+        ruc: this.fb.control('', [Validators.required,Validators.pattern('^[0-9]+$'),Validators.maxLength(13),Validators.minLength(13)]),
+        email: this.fb.control('', [Validators.required,Validators.pattern('^[a-zA-Z0-9_.]+@[a-zA-Z].+[a-zA-Z0-9]$'),Validators.minLength(7)]),
+        razonSocial: this.fb.control('', [Validators.required,Validators.pattern('^[a-zA-Z_ ]+$'),Validators.minLength(5), Validators.maxLength(20)]),
+        telefono: this.fb.control('', [Validators.required,Validators.pattern('^[+_0-9]+$'),Validators.minLength(13),Validators.maxLength(20)]),
+        direccion: this.fb.control('', [Validators.required,Validators.pattern('^[a-zA-Z0-9._ ]+$')]),
 
-      firstName: this.fb.control('', [Validators.required, Validators.pattern('^[a-zA-Z_ ]+$'),Validators.minLength(3),Validators.maxLength(15)]),
-      lastName: this.fb.control('', [Validators.required, Validators.pattern('^[a-zA-Z_ ]+$'),Validators.minLength(3),Validators.maxLength(15)]),
-      cargo: this.fb.control('', [Validators.required, Validators.pattern('^[a-zA-Z_ ]+$'),Validators.minLength(4),Validators.maxLength(35)],),
-      password: this.fb.control('', [Validators.required, Validators.pattern('^[a-zA-Z0-9]+$'),Validators.minLength(6),Validators.maxLength(20)]),
-      confpassword: this.fb.control('', [Validators.required,Validators.pattern('^[a-zA-Z0-9]+$'),Validators.minLength(6),Validators.maxLength(20)]),
+      }),
+      usuario: this.fb.group({
+        firstName: this.fb.control('', [Validators.required, Validators.pattern('^[a-zA-Z_ ]+$'),Validators.minLength(3),Validators.maxLength(15)]),
+        lastName: this.fb.control('', [Validators.required, Validators.pattern('^[a-zA-Z_ ]+$'),Validators.minLength(3),Validators.maxLength(15)]),
+        cargo: this.fb.control('', [Validators.required, Validators.pattern('^[a-zA-Z_ ]+$'),Validators.minLength(4),Validators.maxLength(35)],),
+        password: this.fb.control('', [Validators.required, Validators.pattern('^[a-zA-Z0-9]+$'),Validators.minLength(6),Validators.maxLength(20)]),
+        confpassword: this.fb.control('', [Validators.required,Validators.pattern('^[a-zA-Z0-9]+$'),Validators.minLength(6),Validators.maxLength(20)]),
+        email: this.fb.control('', [Validators.required,Validators.pattern('^[a-zA-Z0-9_.]+@[a-zA-Z].+[a-zA-Z0-9]$'),Validators.minLength(7)]),
+        telefono: this.fb.control('', [Validators.required,Validators.pattern('^[+_0-9]+$'),Validators.minLength(13),Validators.maxLength(20)]),
+        direccion: this.fb.control('', [Validators.required,Validators.pattern('^[a-zA-Z0-9._ ]+$')]),
+      })
     });
   };
 
   ngOnInit(): void {
+  }
+
+  enviar(values:any){
+    console.log(values)
+    this.envio.peticionPost("http://localhost:8000/api/create/",values).toPromise().then(res=>{
+      console.log(res.json())
+    })
   }
 
 }
