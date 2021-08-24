a57a22ba1b5bacb4fced672bcf98011e73090b69
raochoa019 <raochoa@espol.edu.ec>
2021-08-24 18:44:26 -0500

    Validaciones registro

--- Proyecto/src/app/componentes/pagina_comercial/registro/registro.component.ts
+++ Proyecto/src/app/componentes/pagina_comercial/registro/registro.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { FormBuilder, FormGroup, Validators,} from '@angular/forms';
+import { FormBuilder, FormGroup, Validators, } from '@angular/forms';
 
 @Component({
   selector: 'registro',
@@ -8,21 +8,17 @@ import { FormBuilder, FormGroup, Validators,} from '@angular/forms';
 })
 export class RegistroComponent implements OnInit {
   public registroForm: FormGroup;
-  constructor(private fb: FormBuilder) { 
+  constructor(private fb: FormBuilder) {
     this.registroForm = this.fb.group({
-      registro: this.fb.group({
-        razonSocial: this.fb.control('', [Validators.required,Validators.pattern('^[a-zA-Z0-9_ ]+$'),Validators.minLength(5), Validators.maxLength(20)]),
-        nombres: this.fb.control('', [Validators.required, Validators.pattern('^[a-zA-Z ]+$'),Validators.minLength(3),Validators.maxLength(17)]),
-        email: this.fb.control('', [Validators.required,Validators.pattern('^[a-z0-9._%+\-]+@[a-z0-9.\-]+\\.[a-z]{2,4}'),Validators.minLength(7)]),
-        telefono: this.fb.control('', [Validators.required,Validators.pattern('^[+_0-9]+$'),Validators.minLength(13),Validators.maxLength(20)]),
-        direccion: this.fb.control('', [Validators.required,Validators.pattern('^[a-zA-Z0-9._ ]+$')]),
-        cargo: this.fb.control('',[Validators.required,Validators.pattern('^[a-zA-Z ]+$'),Validators.minLength(13),Validators.maxLength(20)]),
-        descripcion: this.fb.control('', [Validators.required,Validators.pattern('^[a-zA-Z ]+$'),Validators.minLength(13),Validators.maxLength(20)])
-      
-      })
-});
+      razonSocial: this.fb.control('', [Validators.required, Validators.pattern('^[a-zA-Z0-9. ]+$'), Validators.minLength(5), Validators.maxLength(20)]),
+      nombre: this.fb.control('', [Validators.required, Validators.pattern('^[a-zA-Z ]+$'), Validators.maxLength(15)]),
+      email: this.fb.control('', [Validators.required, Validators.pattern('^[a-z0-9._%+\-]+@[a-z0-9.\-]+\\.[a-z]{2,4}'), Validators.minLength(7)]),
+      telefono: this.fb.control('', [Validators.required, Validators.pattern('^[+_0-9]+$'), Validators.minLength(13), Validators.maxLength(20)]),
+      cargo: this.fb.control('', [Validators.required, Validators.pattern('^[a-zA-Z ]+$'), Validators.minLength(4), Validators.maxLength(35)],),
+      descripcion: this.fb.control('', [Validators.required, Validators.minLength(10), Validators.maxLength(250)])
+    });
   }
-  
+
   ngOnInit(): void {
   }
 
