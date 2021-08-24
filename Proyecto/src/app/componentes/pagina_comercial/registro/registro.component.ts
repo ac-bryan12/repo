6ec56be5647b418169ee7f781bde24ca706bd2d4
Bryan Alava <bpalava@espol.edu.ec>
2021-08-24 17:52:03 -0500

    Cambios en las validaciones del registro para el envio del correo hacia el admin

--- Proyecto/src/app/componentes/pagina_comercial/registro/registro.component.ts
+++ Proyecto/src/app/componentes/pagina_comercial/registro/registro.component.ts
@@ -10,12 +10,17 @@ export class RegistroComponent implements OnInit {
   public registroForm: FormGroup;
   constructor(private fb: FormBuilder) { 
     this.registroForm = this.fb.group({
-      razonSocial: this.fb.control('', [Validators.required,Validators.pattern('^[a-zA-Z_ ]+$'),Validators.minLength(5), Validators.maxLength(20)]),
-      email: this.fb.control('', [Validators.required,Validators.pattern('^[a-zA-Z0-9_.]+@[a-zA-Z].+[a-zA-Z0-9]$'),Validators.minLength(7)]),
-      telefono: this.fb.control('', [Validators.required,Validators.pattern('^[+_0-9]+$'),Validators.minLength(13),Validators.maxLength(20)]),
-      direccion: this.fb.control('', [Validators.required,Validators.pattern('^[a-zA-Z0-9._ ]+$')]),
-
-    });
+      registro: this.fb.group({
+        razonSocial: this.fb.control('', [Validators.required,Validators.pattern('^[a-zA-Z0-9_ ]+$'),Validators.minLength(5), Validators.maxLength(20)]),
+        nombres: this.fb.control('', [Validators.required, Validators.pattern('^[a-zA-Z ]+$'),Validators.minLength(3),Validators.maxLength(17)]),
+        email: this.fb.control('', [Validators.required,Validators.pattern('^[a-z0-9._%+\-]+@[a-z0-9.\-]+\\.[a-z]{2,4}'),Validators.minLength(7)]),
+        telefono: this.fb.control('', [Validators.required,Validators.pattern('^[+_0-9]+$'),Validators.minLength(13),Validators.maxLength(20)]),
+        direccion: this.fb.control('', [Validators.required,Validators.pattern('^[a-zA-Z0-9._ ]+$')]),
+        cargo: this.fb.control('',[Validators.required,Validators.pattern('^[a-zA-Z ]+$'),Validators.minLength(13),Validators.maxLength(20)]),
+        descripcion: this.fb.control('', [Validators.required,Validators.pattern('^[a-zA-Z ]+$'),Validators.minLength(13),Validators.maxLength(20)])
+      
+      })
+});
   }
   
   ngOnInit(): void {
