a749754f0fb3b03f1b35de9622dcc7f98015ce8d
Bryan Alava <bpalava@espol.edu.ec>
2021-08-26 02:31:46 -0500

    Validaciones extra para el RUC

--- Proyecto/src/app/componentes/pagina_comercial/create-cuenta/create-cuenta.component.ts
+++ Proyecto/src/app/componentes/pagina_comercial/create-cuenta/create-cuenta.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { LoginService } from 'src/app/services/login.service';
-import { FormBuilder, FormGroup, Validators,} from '@angular/forms';
+import { FormBuilder, FormControl, FormGroup, Validators,ValidationErrors,ValidatorFn, AbstractControl, AsyncValidator, AsyncValidatorFn} from '@angular/forms';
 
 
 @Component({
@@ -11,7 +11,6 @@ import { FormBuilder, FormGroup, Validators,} from '@angular/forms';
 
 export class CreateCuentaComponent implements OnInit {
   public createAccount: FormGroup;
-
   msg_content = ''
   msg_d = 'd-none'
   
@@ -21,7 +20,7 @@ export class CreateCuentaComponent implements OnInit {
   ) {
     this.createAccount = this.fb.group({
       empresa: this.fb.group({
-        ruc: this.fb.control('', [Validators.required,Validators.pattern('^[0-9]+$'),Validators.maxLength(13),Validators.minLength(13)]),
+        ruc: this.fb.control('', [Validators.required,Validators.maxLength(13),Validators.minLength(13),]),
         email: this.fb.control('', [Validators.required,Validators.pattern('^[a-z0-9._%+\-]+@[a-z0-9.\-]+\\.[a-z]{2,4}'),Validators.minLength(7)]),
         razonSocial: this.fb.control('', [Validators.required,Validators.pattern('^[a-zA-Z0-9. ]+$'),Validators.minLength(5), Validators.maxLength(20)]),
         telefono: this.fb.control('', [Validators.required,Validators.pattern('^[+_0-9]+$'),Validators.minLength(13),Validators.maxLength(20)]),
@@ -61,6 +60,116 @@ export class CreateCuentaComponent implements OnInit {
       return false;
     }
     return true;
+  }
+
+  validarPN(subcadena:String,subcadena2:String){
+    var arreglo = subcadena.split("")
+    var coeficientes = [2,1,2,1,2,1,2,1,2]
+    var result = []
+    var acum = 0
+    console.log("dentro")
+    for(let i = 0; i<coeficientes.length;i++){
+        var num = Number(arreglo[i])*coeficientes[i]
+        if(num<=9) result[i]=num
+        else result[i]=num-9
+    }
+    for(let j = 0;j<result.length;j++)
+       acum+=result[j]
+    acum = 10 - (acum%10)
+    if(acum === Number(subcadena2))
+      return true
+    return false
+  }
+  validarPJ(subcadena:String,subcadena2:String){
+    var arreglo = subcadena.split("")
+    var coeficientes = [4,3,2,7,6,5,4,3,2]
+    var result = []
+    var acum = 0
+    for(let i = 0; i<coeficientes.length;i++){
+        var num = Number(arreglo[i])*coeficientes[i]
+        result[i]=num
+    }
+    for(let j = 0;j<result.length;j++)
+       acum+=result[j]
+    acum = (acum%11)
+    if(acum != 0) 
+      acum = 11-acum
+    if(acum === Number(subcadena2))
+      return true
+    return false
+  }
+  validarIP(subcadena:String,subcadena2:String){
+    var arreglo = subcadena.split("")
+    var coeficientes = [3,2,7,6,5,4,3,2]
+    var result = []
+    var acum = 0
+    for(let i = 0; i<coeficientes.length;i++){
+        var num = Number(arreglo[i])*coeficientes[i]
+        result[i]=num
+    }
+    for(let j = 0;j<result.length;j++)
+       acum+=result[j]
+    acum =(acum%11)
+    if(acum != 0) 
+      acum = 11-acum
+    if(acum === Number(subcadena2))
+      return true
+    return false
+  }
+  validarRuc(){
+    var ruc = this.createAccount.get(['empresa','ruc'])?.value
+    if (ruc.length === 13) {
+      console.log("Valida 13 digitos")
+      const ultimoDigito = ruc.substring(9, 10);
+      const digitoRegion = ruc.substring(0, 2)
+      if (digitoRegion >= String("01") && digitoRegion <= String("24") || digitoRegion == String(30) && ultimoDigito>=String("1")) {
+          const tercerDigito = ruc.substring(2,3);
+          if(tercerDigito >= String(0) && tercerDigito <= String(5)){
+            //Persona natural
+            var valDecimoDigito = this.validarPN(ruc.substring(0,9),ruc.substring(9,10)) 
+            if(valDecimoDigito){
+              console.log('Persona natural')
+              return true;
+            }
+            else{
+              console.log('Formato incorrecto')
+              return false
+            }  
+          }
+          else if(tercerDigito==String(9)){
+            //Persona juridica falta validar residuo cero
+            var valDecimoDigito = this.validarPJ(ruc.substring(0,9),ruc.substring(9,10)) 
+            if(valDecimoDigito){
+              console.log('Persona juridica')
+              return true;
+            }
+            else{
+              console.log('Formato incorrecto')
+              return false
+            }  
+          }
+          else if(tercerDigito==String(6)){
+            //Entidad publica solo falta valida residuo cero
+            var valDecimoDigito = this.validarIP(ruc.substring(0,8),ruc.substring(8,9)) 
+            if(valDecimoDigito){
+              console.log('Entidad publica')
+              return true;
+            }
+            else{
+              console.log('Formato incorrecto')
+              return false
+            }  
+          }
+          else{
+            return false
+          }
+      } else {
+        return false
+      }
+    } else {
+      return false
+    }
+  
   }
 
 }
