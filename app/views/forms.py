03d7754dc65006cd3818e2c9f9b6596ce231a354
Bryan Alava <bpalava@espol.edu.ec>
2021-09-01 23:01:30 -0500

    Se borraron acrchivos innecesarios

--- app/views/forms.py
+++
@@ -1,9 +0,0 @@
-from django import forms
-from app.models.empresa import Empresa
-
-class EmpresaForms(forms.ModelForm):
-    class Meta:
-        model = Empresa
-        fields = ['ruc','razonSocial','direccion','telefono','correo']
-
-        
