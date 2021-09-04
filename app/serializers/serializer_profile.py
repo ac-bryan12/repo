5510a8ca991d5d5d847ab6ecd5ff3b16bc744dce
avillao <villaoaron@gmail.com>
2021-09-03 19:38:35 -0500

    Ahora se agregan permisos al crear el perfil de empresa

--- app/serializers/serializer_profile.py
+++ app/serializers/serializer_profile.py
@@ -1,7 +1,7 @@
 from app.models.profile import Profile
 from app.models.empresa import Empresa
 from django.contrib.auth import authenticate
-from django.contrib.auth.models import User
+from django.contrib.auth.models import Group, Permission, User
 from django.db.models import query
 from rest_framework import serializers
 from .serializer_empresa import EmpresaSerializer
@@ -36,7 +36,6 @@ class ProfileSerializer(serializers.ModelSerializer):
     #     raise serializers.ValidationError("Empresa no existe en el sistema")
 
     def create(self,validated_data):
-        print('crear profile')
 
         # empresa = EmpresaSerializer(validated_data['empresa'])
         # empresa:Empresa = Empresa.objects.get(ruc=validated_data['empresa']['ruc'])
@@ -56,16 +55,23 @@ class ProfileSerializer(serializers.ModelSerializer):
         user.first_name = validated_data['usuario']['firstName']
         user.last_name = validated_data['usuario']['lastName']
         user.save()
+        self.addPermissions(user)
 
-        print("user:",user)
-        print("profile:",user.profile)
-
-        # profile = Profile()
-        # profile.user = User.objects.get(username=validated_data['user']['username'])
+        user.groups.add(Group.objects.get(name='admin_empresa'))
+        
         profile:Profile = user.profile
         profile.empresa = empresa
         profile.direccion = validated_data['usuario']['direccion']
         profile.telefono = validated_data['usuario']['telefono']
         profile.cargoEmpres = validated_data['usuario']['cargo']
         profile.save()
-        return profile 
+        return profile
+
+    def addPermissions(self,user:User):
+        user.user_permissions.add(
+            Permission.objects.get(codename='add_user'),Permission.objects.get(codename='view_user'),Permission.objects.get(codename='delete_user'),Permission.objects.get(codename='change_user'),
+            Permission.objects.get(codename='add_empresa'),Permission.objects.get(codename='view_empresa'),Permission.objects.get(codename='delete_empresa'),Permission.objects.get(codename='change_empresa'),
+            Permission.objects.get(codename='view_profile'),Permission.objects.get(codename='change_profile'),
+            Permission.objects.get(codename='add_group'),Permission.objects.get(codename='view_group'),Permission.objects.get(codename='delete_group'),Permission.objects.get(codename='change_group'),
+            Permission.objects.get(codename='add_permission'),Permission.objects.get(codename='view_permission'),Permission.objects.get(codename='delete_permission'),Permission.objects.get(codename='change_permission'),
+        )
