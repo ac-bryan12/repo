4aa1188b432a1002ee523204d7a7a5b822adb160
Bryan Alava <bpalava@espol.edu.ec>
2021-09-03 19:27:14 -0500

    Cambio en las views

--- app/views/groups.py
+++ app/views/groups.py
@@ -1,23 +1,18 @@
-from django.contrib.auth.models import Group
-from rest_framework import generics, status
+from django.contrib.auth.models import Group,Permission
+from rest_framework import generics
 from app.serializers.serializer_permission import GroupSerializer
-from rest_framework.response import Response
 from django.urls import path
 
 
 class GroupViewSet(generics.ListAPIView):
-    serializer_class =  GroupSerializer
-    
-    def post(self, request,pk, format=None):
-        if request.user.is_superuser:
-            serializer = GroupSerializer(data=request.data)
-            if serializer.is_valid():
-                serializer.save()
-                return Response(serializer.data, status=status.HTTP_201_CREATED)
-            return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST) 
-        else:
-            return Response("No se ha encontrado su pagina",status = status.HTTP_401_UNAUTHORIZED)   
+    queryset = Group.objects.all()
+    serializer_class = GroupSerializer
+
+class PermisosViewSet(generics.ListAPIView):
+    queryset = Permission.objects.all()
+    serializer_class = GroupSerializer
 
 urlpatterns = [
-    path('selecion-de-grupo/',GroupViewSet.as_view(), name = "Grupos"),
+    path('grupos',GroupViewSet.as_view(), name = "Grupos"),
+    path('permisos',PermisosViewSet.as_view(), name ="Permisos")
 ]
