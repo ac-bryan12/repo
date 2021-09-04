3be1e0655605676f8a7c0367ea7b33df9d7b06d4
Bryan Alava <bpalava@espol.edu.ec>
2021-09-03 19:28:37 -0500

    Se muestra permisos y grupos, falta definir vista para agregar esos cambios

--- Proyecto/src/app/componentes/sitioAdmin/grupos-permisos/grupos-permisos.component.ts
+++ Proyecto/src/app/componentes/sitioAdmin/grupos-permisos/grupos-permisos.component.ts
@@ -9,21 +9,51 @@ import { CookieService } from 'ngx-cookie-service';
 export class GruposPermisosComponent implements OnInit {
   listGrupos:string[]
   listPermisos :string[]
+  listUsuarios :string[]
   constructor(private service:RequestService, private cookie:CookieService) { 
     this.listGrupos = []
     this.listPermisos = []
+    this.listUsuarios = []
   }
 
   ngOnInit(): void {
+    this.listaUsuarios()
   }
 
-  listar(){
-    var cook =this.cookie.get("permissions")
-    console.log(cook)
-    var coo =JSON.parse(decodeURI(cook.replace(/\\054/g, ',')))
-    console.log(JSON.parse(coo)[0]['codename'])
-    // this.listGrupos = []
-    // this.listPermisos = []
+  listaUsuarios(){
+    
+    this.service.peticionGet("http://localhost:8000/api/lista-de-users").subscribe((res)=>{
+      this.listUsuarios = res
+      var campos = document.getElementById("usuarios") as HTMLElement
+      for(let user of res){
+        campos.innerHTML += `<tr>
+                                <td>${user.id}</td>
+                                <td>${user.is_superuser}</td>
+                                <td>${user.username}</td>
+                                <td>${user.email}</td>
+                                <td id="botones-acciones">
+                                    <input id="btn" value="Agregar grupo" type="submit" value="Agregar accion" class="btn btn-primary btn-block">
+                                    <input value ="${user.id}"type="submit" value="Agregar grupo" class="d-none">
+                                </td>
+                            </tr>`
+      }
+          
+    
+    })
+    this.service.peticionGet("http://localhost:8000/api/grupos").subscribe((res)=>{
+      for(let grupo of res){
+        this.listGrupos.push(grupo.name)
+      }
+    })
+   
+    this.service.peticionGet("http://localhost:8000/api/permisos").subscribe((res)=>{
+      for(let grupo of res){
+        this.listPermisos.push(grupo.name)
+      }
+    })
+    var btn = document.getElementById("btn")
+    
   }
 
+  
 }
