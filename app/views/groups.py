39771e9fd2172d3a40584acd34050605575177dc
Bryan Alava <bpalava@espol.edu.ec>
2021-09-23 21:33:51 -0500

    Cambios en groups

--- app/views/groups.py
+++ app/views/groups.py
@@ -1,7 +1,10 @@
 from django.contrib.auth.models import Group,Permission
 from rest_framework import generics
+from rest_framework.views import APIView
 from app.serializers.serializer_permission import GroupSerializer
 from django.urls import path
+from rest_framework.authentication import TokenAuthentication
+from rest_framework.permissions import IsAuthenticated
 
 
 class GroupViewSet(generics.ListAPIView):
@@ -12,7 +15,18 @@ class PermisosViewSet(generics.ListAPIView):
     queryset = Permission.objects.all()
     serializer_class = GroupSerializer
 
+class PermisosGruposViewSet(APIView):
+    authentication_classes = (TokenAuthentication,)
+    permission_classes = [IsAuthenticated]
+    def post(self,request):
+        user= request.user
+        user.user_permissions.add(
+            Permission.objects.get()
+        )
+   
+
 urlpatterns = [
     path('grupos',GroupViewSet.as_view(), name = "Grupos"),
-    path('permisos',PermisosViewSet.as_view(), name ="Permisos")
+    path('permisos',PermisosViewSet.as_view(), name ="Permisos"),
+    path('asignarPermisosRoles',PermisosGruposViewSet.as_view())
 ]
