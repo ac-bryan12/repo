5d010108b534a976ba72329276ac9905ecdf7c22
raochoa019 <raochoa@espol.edu.ec>
2021-10-24 15:34:05 -0500

    Pantalla de carga de componentes de cambio de contraseña

--- frontend/Proyecto/src/app/componentes/sitioAdmin/empresa-temp/empresa-temp.component.ts
+++ frontend/Proyecto/src/app/componentes/sitioAdmin/empresa-temp/empresa-temp.component.ts
@@ -24,14 +24,17 @@ export class EmpresaTempComponent implements OnInit {
           `  
           <tr class="text-center">
             <td>${emp.razonSocial}</td>
+            <td>${emp.nombre}</td>
+            <td>${emp.cargo}</td>
             <td>${emp.telefono}</td>
             <td>${emp.correo}</td>
+            <td><textarea disabled rows="4" class="w-100">${emp.descripcion}</textarea></td>
             <td id="botones-acciones">
-            <input id="${emp.correo}" type="submit" value="Aceptar" class="btn btn-primary btn-block">
-            <input type="submit" value="Eliminar" class="btn btn-danger btn-block">
+            <input id="${emp.correo}" type="submit" value="Aceptar" class="btn btn-primary btn-block">       
             </td>
           </tr>
           `
+          //<input type="submit" value="Eliminar" class="btn btn-danger btn-block">
           contenedor.innerHTML += plantilla
 
           let boton = document.getElementsByTagName("input")
